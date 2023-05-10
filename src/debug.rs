a0298a37d354435e8322ce50412415ea4416ad56
Pietro Atzeni <pietrino.atzeni@gmail.com>
2023-05-10 09:21:41 +0200

    fix: added debug

---
+++ src/debug.rs
@@ -0,0 +1,23 @@
+use lazy_static::lazy_static;
+
+use crate::LayerType;
+
+pub const MAX_ENEMIES: i32 = -1;
+pub const MAX_TILES: i32 = -1;
+
+lazy_static! {
+    pub static ref VALID_LAYERS: Vec<LayerType> = vec![
+        LayerType::Blocks,
+        LayerType::Grass,
+        LayerType::Objects,
+        LayerType::Entities,
+    ];
+}
+
+pub fn can_spawn(current: usize, max: i32) -> bool {
+    if max == -1 {
+        true
+    } else {
+        current <= max as usize
+    }
+}
